91ce56d7e5e131cc6907bd3f166ca1ec3c931de9
mjaiswal <mayank.jaiswal@gemalto.com>
2021-11-05 22:00:16 +0530

    Adding more source files

---
+++ OSSE-Assignment/src/com/bits/wilp/osse/ClassA.java
@@ -0,0 +1,12 @@
+package com.bits.wilp.osse;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Added class A for collaboration");
+
+	}
+
+}
---
+++ OSSE-Assignment/src/com/bits/wilp/osse/ClassC.java
@@ -0,0 +1,13 @@
+package com.bits.wilp.osse;
+
+public class ClassC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Added class C for collaboration");
+
+
+	}
+
+}
