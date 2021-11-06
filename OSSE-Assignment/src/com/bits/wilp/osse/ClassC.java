59f5362c1221f860e750cc910e6690c85f1b451b
schekuribits <92623600+schekuribits@users.noreply.github.com>
2021-11-05 20:33:22 -0700

    Update ClassC.java
    Added code to check whether the given number is a palindrome or not

--- OSSE-Assignment/src/com/bits/wilp/osse/ClassC.java
+++ OSSE-Assignment/src/com/bits/wilp/osse/ClassC.java
@@ -1,13 +1,21 @@
 package com.bits.wilp.osse;
 
-public class ClassC {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Added class C for collaboration");
-
-
-	}
-
-}
+ public class ClassC /*Palindrome*/
+ {  
+  public static void main(String args[])
+  {  
+   int r,sum=0,temp;    
+   int n=454;//It is the number variable to be checked for palindrome  
+   temp=n;    
+   while(n>0)
+   {    
+    r=n%10;  //getting remainder  
+    sum=(sum*10)+r;    
+    n=n/10;    
+   }    
+   if(temp==sum)    
+    System.out.println("palindrome number ");    
+   else    
+    System.out.println("not palindrome");    
+  }  
+ }  
